51c0c7aab2e7c30c834363d1316e520c1fb6512e
Avi Elly <eavi@post.bgu.ac.il>
2018-10-31 21:55:49 +0200

    Add project files.

---
+++ IR_engine/ReadFile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IR_engine
+{
+    class ReadFile
+    {
+        public static List<document> documents = new List<document>();
+        static int docu = 0;
+
+        public static void getDocs(string path)
+        {
+            string[] allfiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string fpath in allfiles)
+            {
+                string f = File.ReadAllText(fpath);
+                addtolist(f);
+            }
+        }
+
+        private static void addtolist(string file)
+        {
+            string[] docs = file.Split(new string[] { "<DOC>" }, StringSplitOptions.None);
+            foreach (string doc in docs)
+                if (doc != "" && doc!="\n") { 
+                docu++;
+                int st = doc.IndexOf("<DOCNO>");
+                int end = doc.IndexOf("</DOCNO");
+                string docNo = doc.Substring(st+7, (end - st)-8).Trim();
+                st = doc.IndexOf("<DATE1>");
+                end = doc.IndexOf("</DATE1>");
+                string date = doc.Substring(st+7, (end - st) - 8).Trim();
+                st = doc.IndexOf("<TI>");
+                end = doc.IndexOf("</TI>");
+                string head = doc.Substring(st+4, (end - st) - 4).Trim();
+                st = doc.IndexOf("<TEXT>");
+                end = doc.IndexOf("</TEXT>");
+                string data = doc.Substring(st+6, (end - st) - 7).Trim();
+                document d = new document(data, docNo, date, head);
+                documents.Add(d);
+            }
+        }
+
+        public static void main()
+        {
+            getDocs("C:\\Users\\zeavi\\Desktop\\TEST1");
+            Console.WriteLine("C:\\Users\\zeavi\\Desktop\\TEST1");
+        }
+    }
+
+}
---
+++ IR_engine/document.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IR_engine
+{
+    class document
+    {
+        private string doc;
+        private string docID;
+        private string docTime;
+        private string docHead;
+
+        public document()
+        {
+            this.doc = null;
+            this.docID = null;
+            this.docTime = null;
+            this.docHead = null;
+        }
+
+        public document(string doc, string docId, string docTime, string docHead)
+        {
+            this.doc = doc;
+            this.docID = docId;
+            this.docTime = docTime;
+            this.docHead = docHead;
+        }
+
+        public void Setdoc(string doc)
+        {
+            this.doc = doc;
+        }
+
+        public void Setdocid(string docID)
+        {
+            this.docID = docID;
+        }
+
+        public void Setdoctime(string docTime)
+        {
+            this.docTime = docTime;
+        }
+        public void Setdochead(string docHead)
+        {
+            this.docHead = docHead;
+        }
+
+    }
+}
