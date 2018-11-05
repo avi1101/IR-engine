9ba2695cfd908c50200b6ae41c4b918fa1d5b3ba
Avi Elly <eavi@post.bgu.ac.il>
2018-11-05 20:19:59 +0200

    returnd fixed num

--- IR_engine/Parse.cs
+++ IR_engine/Parse.cs
@@ -14,7 +14,6 @@ namespace IR_engine
         List<string> pre_terms ;
         HashSet<string> terms = new HashSet<string>();
         private enum months {january,february,march,april,may,june,july,august,september,october,november,december};
-        List<string> terms = new List<string>();
         HashSet<string> stopwords = new HashSet<string>();
         /// <summary>
         /// this is the constructor for the Parser class
@@ -45,8 +44,9 @@ namespace IR_engine
         }
         public void parseText(List<string> words)
         {
-            foreach (string word in words)
+            for (int i = 0; i < words.Count; i++)
             {
+                string word = words[i];
                 if (word == null || word == "" || word == " ")
                     continue;
                 else if (word.Contains("\n"))
@@ -57,11 +57,16 @@ namespace IR_engine
                 }
                 else if (stopwords.Contains(word))
                     continue;
-                else if (IsNumber(word))
+                else if (IsRegNumber(word, i))
                 {
-                    string word2 = NumberSet(word,i,words);
+                    if (IsRegNumber(words[i + 1], i + 1) && words[i + 1].Contains("\\"))
+                    {
+                        string word3 = NumberSet(words[i] + " " + words[i + 1], i + 1, words);
+                        terms.Add(word3);
+                        continue;
+                    }
+                    string word2 = NumberSet(word, i, words);
                     terms.Add(word2);
-
                 }
             }
         }
@@ -84,12 +89,17 @@ namespace IR_engine
         /// </summary>
         /// <param name="input">cheked term</param>
         /// <returns></returns>
-        bool IsNumber(string input)
+        bool IsRegNumber(string input, int idx)
         {
             for (int i = 0; i < input.Length; i++)
             {
-                if (input[i] > '9' || input[i] < '0')
+                if (!Char.IsDigit(input[i]) || input[i] != ',' || input[i] != '.' || input[i] != '\\')
                     return false;
+            }
+            if (pre_terms[idx + 1] == "$" || pre_terms[idx + 1] == "%" || pre_terms[idx + 1] == "percent" || pre_terms[idx + 1] == "percentage" || pre_terms[idx + 1] == "Dollars") { return false; }
+            if (pre_terms[idx + 1] == "Thousand" || pre_terms[idx + 1] == "thousand" || pre_terms[idx + 1] == "Million" || pre_terms[idx + 1] == "million" || pre_terms[idx + 1] == "Billion" || pre_terms[idx + 1] == "Trillion" || pre_terms[idx + 1] == "billion" || pre_terms[idx + 1] == "trillion")
+            {
+                if (pre_terms[idx + 2] == "$" || pre_terms[idx + 2] == "%" || pre_terms[idx + 2] == "percent" || pre_terms[idx + 2] == "percentage" || pre_terms[idx + 2] == "Dollars") { return false; }
             }
             return true;
         }
@@ -100,19 +110,19 @@ namespace IR_engine
         /// <param name="idx"></param>
         /// <param name="words"></param>
         /// <returns></returns>
-        string NumberSet(string input,int idx,List<string>words)
+        string NumberSet(string input, int idx, List<string> words)
         {
             int option = 0;
-            var charsToRemove = new string[] {",", "."};
+            var charsToRemove = new string[] { "," };
             foreach (var c in charsToRemove)
             {
                 input = input.Replace(c, string.Empty);
             }
-            if (words[idx + 1] == "Thousand"){ option = 1; }
-            if (words[idx + 1] == "Million") { option = 2; }
-            if (words[idx + 1] == "Billion" || words[idx + 1] == "Trillion") { option = 3; }
+            if (words[idx + 1] == "Thousand" || words[idx + 1] == "thousand") { option = 1; }
+            if (words[idx + 1] == "Million" || words[idx + 1] == "million") { option = 2; }
+            if (words[idx + 1] == "Billion" || words[idx + 1] == "Trillion" || words[idx + 1] == "billion" || words[idx + 1] == "trillion") { option = 3; }
             double dbl;
-            if(double.TryParse(input, out dbl))
+            if (double.TryParse(input, out dbl))
             {
                 if (option == 1) { dbl = dbl * 1000; }
                 if (option == 2) { dbl = dbl * 1000000; }
@@ -120,19 +130,19 @@ namespace IR_engine
                 if (dbl >= 1000 && dbl < 1000000) { dbl = dbl / 1000 + (dbl % 1000) * (1 / 1000); return (dbl.ToString() + "K"); }
                 else if (dbl >= 1000000 && dbl < 1000000000) { dbl = dbl / 1000000 + (dbl % 1000000) * (1 / 1000000); return (dbl.ToString() + "M"); }
                 else if (dbl >= 1000000000) { dbl = dbl / 1000000000 + (dbl % 1000000000) * (1 / 1000000000); return (dbl.ToString() + "B"); }
-                else {return (dbl.ToString());}
-                }
+                else { return (dbl.ToString()); }
+            }
             int nt;
             if (int.TryParse(input, out nt))
-                {
-                    if (option == 1) { nt = nt * 1000; }
-                    if (option == 2) { nt = nt * 1000000; }
-                    if (option == 3) { nt = nt * 1000000000; }
-                    if (nt >= 1000 && nt < 1000000) { nt = nt / 1000 + (nt % 1000) * (1 / 1000); return (nt.ToString() + "K"); }
-                    else if (nt >= 1000000 && nt < 1000000000) { nt = nt / 1000000 + (nt % 1000000) * (1 / 1000000); return (nt.ToString() + "M"); }
-                    else if (nt >= 1000000000) { nt = nt / 1000000000 + (nt % 1000000000) * (1 / 1000000000); return (nt.ToString() + "B"); }
-                    else { return (nt.ToString()); }
-                }
+            {
+                if (option == 1) { nt = nt * 1000; }
+                if (option == 2) { nt = nt * 1000000; }
+                if (option == 3) { nt = nt * 1000000000; }
+                if (nt >= 1000 && nt < 1000000) { nt = nt / 1000 + (nt % 1000) * (1 / 1000); return (nt.ToString() + "K"); }
+                else if (nt >= 1000000 && nt < 1000000000) { nt = nt / 1000000 + (nt % 1000000) * (1 / 1000000); return (nt.ToString() + "M"); }
+                else if (nt >= 1000000000) { nt = nt / 1000000000 + (nt % 1000000000) * (1 / 1000000000); return (nt.ToString() + "B"); }
+                else { return (nt.ToString()); }
+            }
             long lng;
             if (long.TryParse(input, out lng))
             {
@@ -153,26 +163,14 @@ namespace IR_engine
                 if (dec >= 1000 && dec < 1000000) { dec = dec / 1000 + (dec % 1000) * (1 / 1000); return (dec.ToString() + "K"); }
                 else if (lng >= 1000000 && lng < 1000000000) { dec = dec / 1000000 + (dec % 1000000) * (1 / 1000000); return (dec.ToString() + "M"); }
                 else if (dec >= 1000000000) { dec = dec / 1000000000 + (dec % 1000000000) * (1 / 1000000000); return (dec.ToString() + "B"); }
-                else {
+                else
+                {
                     if (option == 1) { return input + "K"; }
                     if (option == 2) { return input + "M"; }
                     if (option == 3) { return input + "B"; }
+                    else { return input; }
                 }
             }
-            /*
-            string up; string down;
-            int num_up; int num_down; int num;
-            if (input.Contains("\\"))
-            {
-                up = input.Substring(0, input.IndexOf('\\'));
-                down = input.Substring(input.IndexOf('\\'));
-                int.TryParse(up, out num_up);
-                int.TryParse(down, out num_down);
-                num = num_up / num_down;
-                if (option == 1){num = num * 1000;}
-                if (option == 2){num = num * 1000000;}
-                if (option == 3){num = num * 1000000000;}
-                */
             else { return null; }
         }
 
