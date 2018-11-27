01809899f122074e3cc06859f611f74169526b82
Avi Elly <eavi@post.bgu.ac.il>
2018-11-27 19:29:22 +0200

    --added special case

--- IR_engine/model/Parse.cs
+++ IR_engine/model/Parse.cs
@@ -128,6 +128,11 @@ namespace IR_engine
         public HashSet<char> Fixwordlist = new HashSet<char>() { '.','!','?','\n', ',', '|','[',']','(',')','{',
                         '}','&',':','<','>',';'};
         public HashSet<string> percent = new HashSet<string>() { "percent", "PERCENT", "Percent", "percentage", "Percentage", "PERCENTAGE" };
+        public HashSet<string> distance = new HashSet<string>() {"meter","METER","Meter","CM","KM","cm","km","centimeter", "Centimeter", "CENTIMETER", "inch","Inch","INCH",
+              "millimeter","Millimeter","mm","MM","MILLIMETER","Mile","mile","MILE","FEET","Feet","feet","yards","yard","Yard","YARD","Yards","YARDS","decimeter",
+                "Decimeter","DECIMETER","meters","METERS","Meters","centimeters", "Centimeters", "CENTIMETERS", "inches","Inches","INCHES",
+              "millimeters","Millimeters","mm","MM","MILLIMETERS","Miles","miles","MILES","FEETS","Feets","feets","decimeters",
+                "Decimeters","DECIMETERS",};
 
         HashSet<string> stopwords = new HashSet<string>();
         Stemmer stem;
@@ -216,13 +221,16 @@ namespace IR_engine
                 }
                 else if (containsNumbers(word))
                 {
-                    /* 
-                     * the first word contains a number, which means it can apply one of the rules
-                     * here i'll check which rule to apply
-                     */
-
+                    if (Isdistance(words, i))
+                    {
+                        /* 
+                         * the first word contains a number, which means it can apply one of the rules
+                         * here i'll check which rule to apply
+                         */
+                        phrase = Setdistance(words, i, out j);
+                    }
                     // checking for price existance
-                    if (isPrice(words, i))
+                    else if (isPrice(words, i))
                     {
                         /*
                          * we found that word[i] is a price expression
@@ -384,6 +392,13 @@ namespace IR_engine
                 if (dollars.Contains(words[idx + 2]) && (million.Contains(words[idx + 1]) || billion.Contains(words[idx + 1]) || trillion.Contains(words[idx + 1]))) return true;
             if (idx + 1 < words_length)
                 if (dollars.Contains(words[idx + 1]))
+                    return true;
+            return false;
+        }
+        private bool Isdistance(string[] words,int idx)
+        {
+            if (idx + 1 < words_length)
+                if (distance.Contains(words[idx + 1]))
                     return true;
             return false;
         }
@@ -567,6 +582,11 @@ namespace IR_engine
             return sol;
         }
         string SetLetterType(int idx, string[] words) { return null; }
+        string Setdistance(string[] words,int idx,out int j)
+        {
+            j = idx + 1;
+            return words[idx] + words[idx + 1];
+        }
         string Setprice(int idx, string[] words, out int j)
         {
             string val = "";
@@ -834,7 +854,7 @@ namespace IR_engine
         {
             word.Replace("\'", "");
             word.Replace("--", "");
-            bool done = false;
+            //bool done = false;
             //while (!done)
             //{
             //  done = true;
@@ -843,10 +863,9 @@ namespace IR_engine
             else if (word.Length <= 1 && !(Char.IsLetterOrDigit(word[0]))) return "";
             else
             {
-                word = word.TrimEnd('.', '!', '?', '\n', ',', '|', '[', ']', '(', ')', '{',
-                    '}', '&', ':', '<', '>', ';', '-');
-                word.TrimStart('.', '!', '?', '\n', ',', '|', '[', ']', '(', ')', '{',
-                    '}', '&', ':', '<', '>', ';');
+                word = word.Trim('.', '!', '?', '\n', ',', '|', '[', ']', '(', ')', '{',
+                    '}', '&', ':', '<', '>','@','&','*','^','#' ,';',' ');
+                word=word.TrimStart('-');
 
                 //trim//
                 /* if (word[word.Length - 1] == '.' ||word[word.Length-1]=='!'|| word[word.Length - 1] == ',' || word[word.Length - 1] == '\n' || word[word.Length - 1] == '|'||
